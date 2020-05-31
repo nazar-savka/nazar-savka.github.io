633a150a6cc6d661717a05d0b2b961698b289574
nazar-savka <66082863+nazar-savka@users.noreply.github.com>
2020-05-31 17:23:39 +0300

    Add files via upload

---
+++ js/scroll-out-init.js
@@ -0,0 +1,23 @@
+ScrollOut({
+    
+//    once:true,
+    threshold: .3,
+    
+    onShown: function (el) {
+
+        if (el.hasAttribute('data-animation')) {
+            var animationName = el.getAttribute('data-animation');
+            if (!el.classList.contains(animationName))
+                el.classList.add(animationName);
+        }
+    },
+    
+    onHidden: function (el) {
+
+        if (el.hasAttribute('data-animation')) {
+            var animationName = el.getAttribute('data-animation');
+            if (el.classList.contains(animationName))
+                el.classList.remove(animationName);
+        }
+    }
+});
